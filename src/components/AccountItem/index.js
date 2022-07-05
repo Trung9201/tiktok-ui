16acc7eb50efadab982d95ec0a84ea48dd6a9cb2
Admin <nguyenquoctrung0902@gmail.com>
2022-07-05 21:07:26 +0700

    2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx("wrapper")}>
+      <img
+        className={cx("avatar")}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/e3f8d39ce4d0b8446e5f62ab31347daa~c5_100x100.jpeg?x-expires=1657198800&x-signature=L%2FYqa5lhICccJVaQ0crn5J4k6KI%3D"
+        alt="avatar"
+      />
+      <div className={cx("info")}>
+        <p className={cx("name")}>
+          <span>Nguyen Quoc Trung</span>
+          <FontAwesomeIcon className={cx("check")} icon={faCheckCircle} />
+        </p>
+        <span className={cx("username")}>trunghips</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
