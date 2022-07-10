bd2d42440120f1aecb0cb78a1d1606a0b73a4420
Admin <nguyenquoctrung0902@gmail.com>
2022-07-10 16:49:09 +0700

    commit

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -4,6 +4,7 @@ import classNames from "classnames/bind";
 import { faCircleXmark, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import HeadlessTippy from "@tippyjs/react/headless";
 import "tippy.js/dist/tippy.css";
+import { useDebounce } from "~/hooks";
 import styles from "./Search.module.scss";
 
 import { Wrapper as PopperWrapper } from "~/components/Popper";
@@ -18,10 +19,12 @@ function Search() {
   const [showResult, setShowResult] = useState(true);
   const [loading, setLoading] = useState(false);
 
+  const debounced = useDebounce(searchValue, 500);
+
   const inputRef = useRef();
 
   useEffect(() => {
-    if (!searchValue.trim()) {
+    if (!debounced.trim()) {
       setSearchResult([]);
       return;
     }
@@ -29,7 +32,7 @@ function Search() {
 
     fetch(
       `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-        searchValue
+        debounced
       )}&type=less`
     )
       .then((res) => res.json())
@@ -40,7 +43,7 @@ function Search() {
       .catch(() => {
         setLoading(false);
       });
-  }, [searchValue]);
+  }, [debounced]);
 
   const handleClear = () => {
     setSearchValue("");
