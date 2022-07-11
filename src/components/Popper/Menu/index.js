c5c217cb86058112294c183503f094efb1674cca
Admin <nguyenquoctrung0902@gmail.com>
2022-07-11 15:19:52 +0700

    commit

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -10,7 +10,12 @@ const cx = classNames.bind(styles);
 
 const defaultFn = () => {};
 
-function Menu({ children, items = [], onChange = defaultFn }) {
+function Menu({
+  children,
+  items = [],
+  hideOnClick = false,
+  onChange = defaultFn,
+}) {
   const [history, setHistory] = useState([{ data: items }]);
   const current = history[history.length - 1];
 
@@ -37,6 +42,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
       interactive
       delay={[0, 500]}
       offset={[12, 8]}
+      hideOnClick={hideOnClick}
       placement="bottom-end"
       render={(attrs) => (
         <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
