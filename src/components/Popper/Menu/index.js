bcd8a4109fb3410f54c7ee94ac6dd36cee3b6b8f
Admin <nguyenquoctrung0902@gmail.com>
2022-07-13 21:45:49 +0700

    commit

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -38,6 +38,15 @@ function Menu({
       );
     });
   };
+
+  const handleResetToFirstPage = () => {
+    setHistory((prev) => prev.slice(0, 1));
+  };
+
+  const handleBack = () => {
+    setHistory((prev) => prev.slice(0, prev.length - 1));
+  };
+
   return (
     <Tippy
       interactive
@@ -49,18 +58,13 @@ function Menu({
         <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
           <PopperWrapper className={cx("menu-popper")}>
             {history.length > 1 && (
-              <Header
-                title={current.title}
-                onBack={() => {
-                  setHistory((prev) => prev.slice(0, prev.length - 1));
-                }}
-              />
+              <Header title={current.title} onBack={handleBack} />
             )}
             <div className={cx("menu-body")}>{renderItem()}</div>
           </PopperWrapper>
         </div>
       )}
-      onHide={() => setHistory((prev) => prev.slice(0, 1))}
+      onHide={handleResetToFirstPage}
     >
       {children}
     </Tippy>
